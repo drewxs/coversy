c71712db27d36740f4fe4551135c9edd68bf3ef1
kito0 <drewshah0@gmail.com>
2022-01-28 20:02:49 -0700

    Remove unused

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,4 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loginUser } from 'redux/user';
-
 function App() {
-	useEffect(() => {}, [authenticated]);
-
 	return <div className="App"></div>;
 }
 
