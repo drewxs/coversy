d4814982a3c5f01287d5226d1ba395191d546019
Aedan Higgins <Aedan.Higgins@edu.sait.ca>
2022-02-08 09:32:57 -0700

    Added site controllers and routes

---
+++ server/controllers/site.controller.js
@@ -0,0 +1,13 @@
+const Site = require('../models/site.model');
+
+exports.createSite = async (req, res) => {};
+
+exports.getAllSites = async (req, res) => {};
+
+exports.getSiteById = async (req, res) => {};
+
+exports.getSiteByName = async (req, res) => {};
+
+exports.updateSiteById = async (req, res) => {};
+
+exports.deleteSiteById = async (req, res) => {};
---
+++ server/routes/site.route.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Router } = require('express');
+const {
+	createSite,
+	getAllSites,
+	getSiteById,
+	getSiteByName,
+	updateSiteById,
+	deleteSiteById,
+} = require('../controllers/site.controller');
+
+// CREATE
+router.post('/', createSite);
+
+// READ
+router.get('/', getAllSites);
+router.get('/:siteID', getSiteById);
+router.get('/:siteName', getSiteByName);
+
+// UPDATE
+router.post('/:siteID', updateSiteById);
+
+// DELETE
+router.post('/:siteID', deleteSiteById);
+
+module.exports = router;
