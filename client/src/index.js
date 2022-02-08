902c1bbc9bade046adbe83372726d1a050e8313a
kito0 <drewshah0@gmail.com>
2022-02-07 22:25:41 -0700

    Resolve sass errors

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from 'App';
-import 'sass/main.scss';
+import './sass/main.scss';
 import store from 'redux/store';
 
 ReactDOM.render(
