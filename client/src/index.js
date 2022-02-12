de6e99b167fce34ed288c95746c97fa5172f74e5
kito0 <drewshah0@gmail.com>
2022-02-12 00:54:12 -0700

    Add material-ui config

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { App } from 'App';
+import { ThemeProvider } from '@mui/material/styles';
+
 import 'scss/main.scss';
+import { App } from 'App';
+import { theme } from 'config/theme';
 import store from 'redux/store';
 
 ReactDOM.render(
-	<Router>
-		<Provider store={store}>
+	<Provider store={store}>
+		<ThemeProvider theme={theme}>
 			<App />
-		</Provider>
-	</Router>,
+		</ThemeProvider>
+	</Provider>,
 	document.getElementById('root')
 );
---
+++ client/src/pages/Stylesheet.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Stylesheet = () => {
+	return (
+		<>
+			<h1>
+				Heading 1<br /> Second Line
+			</h1>
+			<h2>
+				Heading 2<br /> Second Line
+			</h2>
+			<h3>
+				Heading 3<br /> Second Line
+			</h3>
+			<h4>
+				Heading 4<br /> Second Line
+			</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Cupiditate, dolore! Reiciendis dolore beatae laborum
+				necessitatibus consectetur assumenda modi cumque libero?
+			</p>
+		</>
+	);
+};
