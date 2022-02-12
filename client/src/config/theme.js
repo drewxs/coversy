8129cae52563a4f954436256569fd0cf432fbc2e
kito0 <drewshah0@gmail.com>
2022-02-12 00:04:58 -0700

    Add mui

---
+++ client/src/config/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+	typography: {
+		fontFamily: 'Inter, sans-serif',
+	},
+	palette: {
+		primary: '#e57373',
+		secondary: '#61cc90',
+	},
+});
