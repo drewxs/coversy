1048eccf7ef11ec7e16fba89e24d111939b91807
kito0 <drewshah0@gmail.com>
2022-02-12 00:55:05 -0700

    Update imports

---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { Login } from 'pages/Login';
+export { Profile } from './Profile';
+export { Registration } from './Registration';
+export { SiteRegistration } from './SiteRegistration';
+export { Stylesheet } from './Stylesheet';
