760365f30b9ebd91157f366c0bab2c5e7c82161f
Trung Hoang <huytrung.hoang@edu.sait.ca>
2022-02-14 00:54:12 +0700

    Add Registration and Registration  Institution

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {
-	Login,
-	Profile,
-	Registration,
-	SiteRegistration,
-	Stylesheet,
-} from 'pages';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Profile } from 'pages/Profile';
+import { Login } from 'pages/Login';
+import { Registration } from 'pages/Registration';
+import { RegistrationUser } from 'pages/RegistrationUser';
 
 export const App = () => {
+	
 	return (
-		<Router>
-			<Routes>
-				<Route exact path='/login' element={<Login />} />
-				<Route exact path='/profile' element={<Profile />} />
-				<Route exact path='/registration' element={<Registration />} />
-				<Route
-					exact
-					path='/registration/site'
-					element={<SiteRegistration />}
-				/>
-				<Route exact path='/stylesheet' element={<Stylesheet />} />
-			</Routes>
-		</Router>
+		<div>
+			<Registration/> 
+			<RegistrationUser/> 
+		</div>
 	);
 };
