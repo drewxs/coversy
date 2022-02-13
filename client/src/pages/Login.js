f7be4ccd10336f492e9201d23eb29ae6593ca003
kito0 <drewshah0@gmail.com>
2022-02-12 22:35:48 -0700

    Update Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,42 +1,46 @@
 import React from 'react';
 
 export const Login = () => {
-	return ( 
-	<div className="main">
-	<div className="sub-main">
-	  <div>
-		<div className="imgs">
-		  <div className="container-imgs">
-			<img></img>
-		  </div>
-
-
+	return (
+		<div className='main'>
+			<div className='sub-main'>
+				<div>
+					<div className='imgs'>
+						<div className='container-imgs'>
+							<img></img>
+						</div>
+					</div>
+					<div>
+						<div className='usrpass'>
+							<input
+								type='text'
+								placeholder='Email'
+								className='name'
+							></input>
+						</div>
+						<div className='usrpass'>
+							<input
+								type='password'
+								placeholder='Password'
+								className='name'
+							></input>
+						</div>
+						<div className='forgotPass'>
+							<a href='#'>Forgot password?</a>
+						</div>
+						<div className='loginButton'>
+							<button>
+								<p>Login</p>
+							</button>
+							<div className='newUser'>
+								<p>
+									<a href='#'>New User?</a> Register Here
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-
-		<div>
-		  <div className="usrpass">
-			<input type="text" placeholder="Emai" className="name"></input>
-		  </div>
-
-		  <div className="usrpass">
-			<input type="password" placeholder="Password" className="name"></input>
-		  </div>
-
-		  <div className="forgotPass">
-			<a href="#">Forgot password?</a>
-		  </div>
-
-		  <div className="loginButton">
-			<button><p>Login</p></button>
-			<div className="newUser">
-			<p><a href="#">New User?</a> Register Here</p>
-		  </div>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </div>
-  );
+	);
 };
-
-export default Login;
