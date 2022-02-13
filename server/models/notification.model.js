816d069b396777d0f711051710ab3b9797a979dd
Aedan Higgins <Aedan.Higgins@edu.sait.ca>
2022-02-12 22:01:03 -0700

    Add notifications

---
+++ server/models/notification.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const NotificationSchema = new mongoose.Schema(
+	{
+		sender: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		receiver: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		message: {
+			type: String,
+			required: true,
+			max: 128,
+		},
+		read: {
+			type: Boolean,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
