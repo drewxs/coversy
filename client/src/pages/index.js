b78bf8e7044cb48f8a5a0de93d935f0b0004365c
kito0 <drewshah0@gmail.com>
2022-02-14 02:06:19 -0700

    Remove stylesheet

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,4 +2,3 @@ export { Login } from 'pages/Login';
 export { Profile } from './Profile';
 export { Registration } from './Registration';
 export { SiteRegistration } from './SiteRegistration';
-export { Stylesheet } from './Stylesheet';
