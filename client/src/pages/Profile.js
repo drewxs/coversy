75713622552e46d25d5ce5e5949a79b945f6f891
Zhenyi <Zhenyi@DESKTOP-IT8GUBP>
2022-02-14 10:57:09 -0700

    profile demo

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+
+
+
+
+
 
 export const Profile = () => {
+	const [user, setUser] = useState('');
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [middleInitial, setMiddleInitial] = useState('');
+	const [profileimg, setProfileimg] = useState('');
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [site, setSite] = useState('');
+	const [phone, setPhone] = useState('');
+
+
+	
 	// const user = {
 	// 	firstName: 'John',
 	// 	lastName: 'Doe',
@@ -10,11 +30,29 @@ export const Profile = () => {
 	// 	phone: '1231231231',
 	// };
 
-	return (
-		<>
-			<section>
-				<div className='container'></div>
-			</section>
-		</>
+
+
+
+	return {
+		setData: (user) => {
+			let fullname = `${user.firstName} ${user.middleInitial} ${user.lastName}`;
+
+			profileimg.src = user.picture.large;
+			name.textContent = fullname;
+			email.textContent = user.email;
+			phone.textContent = user.phone;
+			site.textContent = user.site;
+		}
+	}
+})(
 	);
-};
+
+
+
+fetch('--------')
+	.then((resp) => resp.json())
+	.then((user) => {
+		Profile.setUser(user.results[0]);
+	});
+
+
