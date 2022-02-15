81a3c946921d4fe37e165d5f8022cc8c6d87ab01
Zhenyi <Zhenyi@DESKTOP-IT8GUBP>
2022-02-15 08:43:51 -0700

    profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,126 +1,87 @@
-import React from 'react';
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import ButtonBase from "@mui/material/ButtonBase";
-import Avatar from "@mui/material/Avatar";
-import Divider from "@mui/material/Divider";
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import React from "react";
+import {
+    Grid,
+    Paper,
+    ButtonBase,
+    Avatar,
+    Divider,
+    FormGroup,
+    FormControlLabel,
+    Checkbox,
+    ToggleButton,
+    ToggleButtonGroup,
+} from "@mui/material";
 
-
-
-
+import { useSelector } from "react-redux";
 
 export const Profile = () => {
+    const user = useSelector((state) => state.userSlice.user);
 
-	const user = useSelector((state) => state.userSlice.user);
+    return (
+        <Paper
+            sx={{
+                p: 10,
+                margin: "auto",
+                maxWidth: 500,
+                flexGrow: 1,
+            }}
+        >
+            <Grid container spacing={12}>
+                <Grid item>
+                    <ButtonBase sx={{ width: 128, height: 128 }}>
+                        <Avatar alt="profileimg" src="/userImage" />
+                    </ButtonBase>
+                    <div className="header">
+                        <h2>
+                            {user.firstName} {user.middleInitial}{" "}
+                            {user.lastName}{" "}
+                        </h2>
+                    </div>
+                    <Divider variant="large" />
 
-	const profileimg = user.profileimg;
-	
-	const site = user.site;
-	const email = user.email;
-	const phone = user.phone;
+                    <h3>Institute:</h3>
+                    <div className="info">
+                        {" "}
+                        <p>{user.site}</p>
+                    </div>
+                    <div className="info">
+                        <p>{user.location}</p>
 
-	let name = `${user.firstName} ${user.middleInitial} ${user.lastName}`;
+                        <p>{user.phone}</p>
 
-
-	
-	// const user = {
-	// 	firstName: 'John',
-	// 	lastName: 'Doe',
-	// 	middleInitial: 'M',
-	// 	email: 'johndoe@gmail.com',
-	// 	site: 'Ogden',
-	// 	phone: '1231231231',
-	// };
-
-
-
-
-	return {
-		setData: (user) => {
-			
-
-			profileimg.src = user.picture;
-			name.textContent = fullname;
-			email.textContent = user.email;
-			phone.textContent = user.phone;
-			site.textContent = user.site;
-		}
-	}
-})
-
-
-
-
-
-
-export default function Profile() {
-        return (
-            <Paper
-                sx={{
-                    p: 10,
-                    margin: "auto",
-                    maxWidth: 500,
-                    flexGrow: 1,
-                    backgroundColor: (theme) =>
-                        theme.palette.mode === "dark" ? "#1A2027" : "#fff"
-                }}
-            >
-                <Grid container spacing={12}>
-                    <Grid item>
-                        <ButtonBase sx={{ width: 128, height: 128 }}>
-                            <Avatar alt="profileimg" src="/userImage" />
-                        </ButtonBase>
-                        <div class="header">
-                            <h2 class="name"></h2>
-                        </div>
-                        <Divider variant="large" />
-
-                        <h3>Institute:</h3>
-                        <div class="info">
-                            {" "}
-                            <p class="site"></p>
-                        </div>
-                        <div class="info">
-                            <p class="location"></p>
-
-                            <p class="cell"></p>
-
-                            <p class="email"></p>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} sm container>
-                        <Grid item xs container direction="column" spacing={2}>
-                            <Grid item xs>
-                                <ToggleButtonGroup
-
-                                    aria-label="text formatting"
+                        <p>{user.Email}</p>
+                    </div>
+                </Grid>
+                <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                            <ToggleButtonGroup aria-label="text formatting">
+                                <ToggleButton
+                                    value="Notifications"
+                                    label="Notifications"
                                 >
-                                    <ToggleButton value="Notifications" label="Notifications">
-                                        Notifications
-                                    </ToggleButton>
-                                    <ToggleButton value="EditProfile" label="Edit">
-                                        Edit Profile
-                                    </ToggleButton>
-                                </ToggleButtonGroup>
-                            </Grid>
-                            <Grid item>
-                                <FormGroup>
-                                    <FormControlLabel control={<Checkbox defaultChecked />} label="Reveive notification to your email" />
-                                    <FormControlLabel control={<Checkbox />} label="Send me text message notifaction" />
-                                </FormGroup>
-                            </Grid>
+                                    Notifications
+                                </ToggleButton>
+                                <ToggleButton value="EditProfile" label="Edit">
+                                    Edit Profile
+                                </ToggleButton>
+                            </ToggleButtonGroup>
+                        </Grid>
+                        <Grid item>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={<Checkbox defaultChecked />}
+                                    label="Reveive notification to your email"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox />}
+                                    label="Send me text message notifaction"
+                                />
+                            </FormGroup>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Paper>
-        );
-    }
-
-
+            </Grid>
+        </Paper>
+    );
+};
