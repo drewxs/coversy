020f98aaec170dd1c57e64f38e8aa661f3e3c980
Tomnguyen2604 <63090801+Tomnguyen2604@users.noreply.github.com>
2022-02-15 22:20:24 -0700

    Update on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "assets/logo.jpg";
+import avatar from "assets/logo2.png";
 import {
     Grid,
     Paper,
@@ -18,70 +20,20 @@ export const Profile = () => {
     const user = useSelector((state) => state.userSlice.user);
 
     return (
-        <Paper
-            sx={{
-                p: 10,
-                margin: "auto",
-                maxWidth: 500,
-                flexGrow: 1,
-            }}
-        >
-            <Grid container spacing={12}>
-                <Grid item>
-                    <ButtonBase sx={{ width: 128, height: 128 }}>
-                        <Avatar alt="profileimg" src="/userImage" />
-                    </ButtonBase>
-                    <div className="header">
-                        <h2>
-                            {user.firstName} {user.middleInitial}{" "}
-                            {user.lastName}{" "}
-                        </h2>
+        <section className="profile">
+            <div className="card container">
+                <div className="row">
+                    <div className="col-profile">
+                        <Avatar src={avatar} alt="avatar" />
+                        <h1>
+                            {user.firstName} {user.lastName}
+                        </h1>
+                        <h3>{user.phone}</h3>
+                        <h3>{user.email}</h3>
                     </div>
-                    <Divider variant="large" />
-
-                    <h3>Institute:</h3>
-                    <div className="info">
-                        {" "}
-                        <p>{user.site}</p>
-                    </div>
-                    <div className="info">
-                        <p>{user.location}</p>
-
-                        <p>{user.phone}</p>
-
-                        <p>{user.Email}</p>
-                    </div>
-                </Grid>
-                <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                        <Grid item xs>
-                            <ToggleButtonGroup aria-label="text formatting">
-                                <ToggleButton
-                                    value="Notifications"
-                                    label="Notifications"
-                                >
-                                    Notifications
-                                </ToggleButton>
-                                <ToggleButton value="EditProfile" label="Edit">
-                                    Edit Profile
-                                </ToggleButton>
-                            </ToggleButtonGroup>
-                        </Grid>
-                        <Grid item>
-                            <FormGroup>
-                                <FormControlLabel
-                                    control={<Checkbox defaultChecked />}
-                                    label="Reveive notification to your email"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox />}
-                                    label="Send me text message notifaction"
-                                />
-                            </FormGroup>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Paper>
+                    <div className="col"></div>
+                </div>
+            </div>
+        </section>
     );
 };
