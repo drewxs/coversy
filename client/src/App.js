7dbc788db640c30298ca684c3095d3891e05a40d
kito0 <drewshah0@gmail.com>
2022-02-15 23:01:15 -0700

    Update client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,48 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home, Login, Profile, Register, RegisterSite, Welcome } from 'pages';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Home, Login, Profile, Register, SiteRegister, Welcome } from 'pages';
 
 export const App = () => {
+	const authenticated = useSelector((state) => state.userSlice.authenticated);
+
 	return (
 		<Router>
 			<Routes>
 				<Route exact path='/' element={<Home />} />
+				{authenticated && (
+					<>
+						<Route exact path='/profile' element={<Profile />} />
+						<Route
+							exact
+							path='/login'
+							element={<Navigate to='/profile' />}
+						/>
+						<Route
+							exact
+							path='/register'
+							element={<Navigate to='/profile' />}
+						/>
+						<Route
+							exact
+							path='/register/site'
+							element={<Navigate to='/profile' />}
+						/>
+					</>
+				)}
 				<Route exact path='/login' element={<Login />} />
-				<Route exact path='/profile' element={<Profile />} />
 				<Route exact path='/register' element={<Register />} />
-				<Route exact path='/register/site' element={<RegisterSite />} />
+				<Route exact path='/register/site' element={<SiteRegister />} />
 				<Route
 					path='/confirm/:confirmationCode'
 					element={<Welcome />}
 				/>
+				<Route path='*' element={<Navigate to='/login' />} />
 			</Routes>
 		</Router>
 	);
