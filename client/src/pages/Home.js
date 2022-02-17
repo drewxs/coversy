7253761033fc258dae1f425231873a4b50cb801b
kito0 <drewshah0@gmail.com>
2022-02-17 01:21:18 -0700

    Update home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import HeroImg from 'assets/hero.jpg';
 
 export const Home = () => {
 	return (
@@ -9,29 +10,45 @@ export const Home = () => {
 					<h2>Coversy</h2>
 					<div className='button-cont'>
 						<Button
-							variant='contained'
-							color='primary'
+							variant='outlined'
+							color='secondary'
 							href='/register'
 							className='button'
 						>
 							Register
 						</Button>
+
 						<Button
-							variant='contained'
-							color='primary'
-							href='/register/site'
-							className='button'
-						>
-							Register Site
-						</Button>
-						<Button
-							variant='contained'
-							color='primary'
+							variant='outlined'
+							color='secondary'
 							href='/login'
 							className='button'
 						>
 							Login
 						</Button>
+					</div>
+				</div>
+			</section>
+			<section className='hero'>
+				<div className='container'>
+					<div className='col left'>
+						<h3>
+							Lorem ipsum, dolor sit amet consectetur adipisicing
+							elit. Est quos obcaecati unde officia voluptatem eos
+							nesciunt voluptatum libero laboriosam numquam?
+						</h3>
+						<Button
+							className='button'
+							variant='contained'
+							color='primary'
+							href='/register/site'
+							size='large'
+						>
+							Register New Site
+						</Button>
+					</div>
+					<div className='col'>
+						<img src={HeroImg} alt='Woman looking at clock.' />
 					</div>
 				</div>
 			</section>
