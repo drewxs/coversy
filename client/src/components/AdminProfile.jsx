b22b4b6517f39992cbd3e4e270247f64ed4a96a2
Tomnguyen2604 <63090801+Tomnguyen2604@users.noreply.github.com>
2022-02-16 20:56:28 -0700

    Updated on Admin Profile
    Co-authored-by: Zhenyi <zhenyi943@users.noreply.github.com>

--- client/src/components/AdminProfile.jsx
+++ client/src/components/AdminProfile.jsx
@@ -1,10 +1,193 @@
 import React from "react";
 
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Switch,
+} from "@mui/material";
+import { Box, Tab, TabContext, TabList, TabPanel } from "@mui/material";
+
 export const AdminProfile = () => {
+    const users = [
+        {
+            firstName: "John",
+            lastName: "Doe",
+            email: "johndoe@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bob",
+            lastName: "Doe",
+            email: "bobdoe@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Robert",
+            lastName: "Doe",
+            email: "robdoe@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Foo",
+            lastName: "Bar",
+            email: "foobar@gmail.com",
+            activated: true,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Foo",
+            email: "barfoo@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Foo",
+            lastName: "Foo",
+            email: "foofoo@gmail.com",
+            activated: true,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+        {
+            firstName: "Bar",
+            lastName: "Bar",
+            email: "bar@gmail.com",
+            activated: false,
+        },
+    ];
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
     return (
-        <div>
+        <div className="admin-profile">
             User Activation
             <h2>Users</h2>
+            <div className="table-cont">
+                <Table className="table" stickyHeader>
+                    <TableHead className="tablehead">
+                        <TableRow>
+                            <TableCell> </TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Activation</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users.map((user, k) => (
+                            <TableRow key={k}>
+                                <TableCell>{k + 1}</TableCell>
+                                <TableCell>
+                                    {user.firstName} {user.lastName}
+                                </TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>
+                                    <Switch checked={user.activated} />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     );
 };
