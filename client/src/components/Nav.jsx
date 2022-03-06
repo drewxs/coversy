9dc9e45b46eb96002dcbc1283565bb49c63f1515
kito0 <drewshah0@gmail.com>
2022-03-06 14:24:26 -0700

    Update Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const Nav = () => {
+	const authenticated = useSelector((state) => state.user.authenticated);
+	const user = useSelector((state) => state.user.user);
+
 	return <div></div>;
 };
