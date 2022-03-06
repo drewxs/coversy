d85df004cdabf02a228279ffa9a7d036459f41d1
Tomnguyen2604 <63090801+Tomnguyen2604@users.noreply.github.com>
2022-03-05 22:44:18 -0700

    update on Payroll

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-} from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { Home, Login, Profile, Register, SiteRegister, Welcome } from 'pages';
-import { LoadUser } from 'redux/user';
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+    Home,
+    Login,
+    Profile,
+    Register,
+    SiteRegister,
+    Welcome,
+    PayrollAdmin,
+} from "pages";
+import { LoadUser } from "redux/user";
 
 export const App = () => {
-	const authenticated = useSelector((state) => state.userSlice.authenticated);
+    const authenticated = useSelector((state) => state.userSlice.authenticated);
 
-	useEffect(() => {
-		if (authenticated) LoadUser();
-	}, [authenticated]);
+    useEffect(() => {
+        if (authenticated) LoadUser();
+    }, [authenticated]);
 
-	return (
-		<Router>
-			<Routes>
-				{authenticated && (
-					<>
-						<Route
-							exact
-							path='/'
-							element={<Navigate to='/profile' />}
-						/>
-						<Route exact path='/profile' element={<Profile />} />
-						<Route
-							exact
-							path='/login'
-							element={<Navigate to='/profile' />}
-						/>
-						<Route
-							exact
-							path='/register'
-							element={<Navigate to='/profile' />}
-						/>
-						<Route
-							exact
-							path='/register/site'
-							element={<Navigate to='/profile' />}
-						/>
-					</>
-				)}
-				<Route exact path='/' element={<Home />} />
-				<Route exact path='/login' element={<Login />} />
-				<Route exact path='/register' element={<Register />} />
-				<Route exact path='/register/site' element={<SiteRegister />} />
-				<Route
-					path='/confirm/:confirmationCode'
-					element={<Welcome />}
-				/>
-				<Route path='*' element={<Navigate to='/login' />} />
-			</Routes>
-		</Router>
-	);
+    return (
+        <Router>
+            <Routes>
+                {authenticated && (
+                    <>
+                        <Route
+                            exact
+                            path="/"
+                            element={<Navigate to="/profile" />}
+                        />
+                        <Route exact path="/profile" element={<Profile />} />
+                        <Route
+                            exact
+                            path="/login"
+                            element={<Navigate to="/profile" />}
+                        />
+                        <Route
+                            exact
+                            path="/register"
+                            element={<Navigate to="/profile" />}
+                        />
+                        <Route
+                            exact
+                            path="/register/site"
+                            element={<Navigate to="/profile" />}
+                        />
+                        <Route
+                            exact
+                            path="/PayrollAdmin"
+                            element={<Navigate to="/PayrollAdmin" />}
+                        />
+                    </>
+                )}
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/register" element={<Register />} />
+                <Route exact path="/register/site" element={<SiteRegister />} />
+                <Route
+                    path="/confirm/:confirmationCode"
+                    element={<Welcome />}
+                />
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
 };
