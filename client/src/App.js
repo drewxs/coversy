3730c9dafb1742f05357e14c4ee750c7a58b1d6e
kito0 <drewshah0@gmail.com>
2022-03-06 19:26:42 -0700

    Update routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,84 @@
 import React, { useEffect } from 'react';
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
 } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import {
-	Home,
-	Login,
-	Profile,
-	Register,
-	SiteRegister,
-	Welcome,
-	PayrollAdmin,
-	DashboardAdmin,
+    Home,
+    Login,
+    Profile,
+    Register,
+    SiteRegister,
+    Welcome,
+    PayrollAdmin,
+    DashboardAdmin,
 } from 'pages';
 import { LoadUser } from 'redux/user';
 
 export const App = () => {
-	const authenticated = useSelector((state) => state.user.authenticated);
-	const user = useSelector((state) => state.user.user);
+    const authenticated = useSelector((state) => state.user.authenticated);
+    const user = useSelector((state) => state.user.user);
 
-	useEffect(() => {
-		if (authenticated) LoadUser();
-	}, [authenticated]);
+    useEffect(() => {
+        if (authenticated) LoadUser();
+    }, [authenticated]);
 
-	return (
-		<Router>
-			<Routes>
-				{/* Admin routes */}
-				{authenticated && user.type === 1 && (
-					<>
-						<Route
-							exact
-							path='/payroll'
-							element={<PayrollAdmin />}
-						/>
-						<Route
-							exact
-							path='/home'
-							element={<DashboardAdmin />}
-						/>
-					</>
-				)}
+    return (
+        <Router>
+            <Routes>
+                {/* Admin routes */}
+                {authenticated && user.type === 1 && (
+                    <>
+                        <Route
+                            exact
+                            path='/payroll'
+                            element={<PayrollAdmin />}
+                        />
+                        <Route
+                            exact
+                            path='/home'
+                            element={<DashboardAdmin />}
+                        />
 
-				{/* User routes */}
-				{authenticated && (
-					<>
-						<Route exact path='/profile' element={<Profile />} />
+                        {/* Redirects */}
+                        <Route path='*' element={<Navigate to='/home' />} />
+                    </>
+                )}
 
-						{/* Redirects */}
-						<Route path='*' element={<Navigate to='/profile' />} />
-					</>
-				)}
+                {/* User routes */}
+                {authenticated && user.type === 2 && (
+                    <>
+                        <Route exact path='/profile' element={<Profile />} />
 
-				{/* Global routes */}
-				<Route exact path='/' element={<Home />} />
-				<Route exact path='/login' element={<Login />} />
-				<Route exact path='/register' element={<Register />} />
-				<Route exact path='/register/site' element={<SiteRegister />} />
-				<Route
-					path='/confirm/:confirmationCode'
-					element={<Welcome />}
-				/>
+                        {/* Redirects */}
+                        <Route path='*' element={<Navigate to='/profile' />} />
+                    </>
+                )}
 
-				{/* Unspecified routes */}
-				<Route path='*' element={<Navigate to='/login' />} />
-			</Routes>
-		</Router>
-	);
+                {/* Global routes */}
+                {!authenticated && (
+                    <>
+                        <Route exact path='/' element={<Home />} />
+                        <Route exact path='/login' element={<Login />} />
+                        <Route exact path='/register' element={<Register />} />
+                        <Route
+                            exact
+                            path='/register/site'
+                            element={<SiteRegister />}
+                        />
+                        <Route
+                            path='/confirm/:confirmationCode'
+                            element={<Welcome />}
+                        />
+
+                        {/* Redirects */}
+                        <Route path='*' element={<Navigate to='/' />} />
+                    </>
+                )}
+            </Routes>
+        </Router>
+    );
 };
