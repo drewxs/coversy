b73f4333e2a850ef039594d9ae20a77dd85a6ec5
Aedan Higgins <Aedan.Higgins@edu.sait.ca>
2022-03-07 13:41:58 -0700

    Update read notification route

--- server/routes/notification.route.js
+++ server/routes/notification.route.js
@@ -3,12 +3,12 @@ const {
 	getNotificationsByUserId,
 	readNotification,
 } = require('../controllers/notification.controller');
-const { verifyUser, verifyAdmin } = require('../middleware/verify');
+const verifyUser = require('../middleware/verify');
 
 // READ
 router.get('/:userId', verifyUser, getNotificationsByUserId);
 
 // UPDATE
-router.post('/:userId/:notificationId', verifyUser, readNotification);
+router.put('/:userId/:notificationId', verifyUser, readNotification);
 
 module.exports = router;
