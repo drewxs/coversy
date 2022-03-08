e49b10333022b880b50f98493cf840b2a0570c85
kito0 <drewshah0@gmail.com>
2022-03-07 20:59:57 -0700

    Update Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,72 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Button } from '@mui/material';
+import { LogoutUser } from 'redux/user';
 
 export const Nav = () => {
-	return <div></div>;
+    const authenticated = useSelector((state) => state.user.authenticated);
+    const user = useSelector((state) => state.user.user);
+
+    return (
+        <section className='nav'>
+            <div className='container'>
+                <h2>Coversy</h2>
+                <div className='button-cont'>
+                    {!authenticated && (
+                        <>
+                            <Button
+                                variant='outlined'
+                                color='secondary'
+                                href='/register'
+                                className='button'
+                            >
+                                Register
+                            </Button>
+
+                            <Button
+                                variant='outlined'
+                                color='secondary'
+                                href='/login'
+                                className='button'
+                            >
+                                Login
+                            </Button>
+                        </>
+                    )}
+
+                    {authenticated && user.type === 1 && (
+                        <>
+                            <Button
+                                variant='outlined'
+                                color='secondary'
+                                href='/home'
+                                className='button'
+                            >
+                                Dashboard
+                            </Button>
+                            <Button
+                                variant='outlined'
+                                className='button logout-btn'
+                                href='/payroll'
+                            >
+                                Payroll
+                            </Button>
+                        </>
+                    )}
+
+                    <Button
+                        variant='outlined'
+                        className='button logout-btn'
+                        onClick={() => {
+                            <Navigate to='/login' />;
+                            LogoutUser();
+                        }}
+                    >
+                        Logout
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
 };
