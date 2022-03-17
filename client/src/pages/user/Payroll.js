6530ca22b1126c088381ce8aa3fd3a3da4cbe683
kito0 <drewshah0@gmail.com>
2022-03-17 08:17:12 -0600

    Add User Payroll

--- client/src/pages/user/Payroll.js
+++ client/src/pages/user/Payroll.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import { UserPayrollReportForm } from 'components/UserPayrollReportForm';
-import { UserPayrollTable } from 'components/UserPayrollTable';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import {
+    Button,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+} from '@mui/material';
+import { GetUserPayrolls } from 'redux/payroll';
 
 export const Payroll = () => {
+    const payrolls = useSelector((state) => state.payroll.payrolls);
+
+    useEffect(() => {
+        GetUserPayrolls();
+    }, []);
+
     return (
-        <div className='main-container'>
-            <section className='payroll'>
-                <div className='payroll-info'>
-                    <h1>My Payroll</h1>
-                    <h3 id='userName'>Name:</h3>
-                    <h3 id='userAnnualTotal'>Annual Total:</h3>
-                    <h3 id='userWage'>Wage: </h3>
-                    <h3 id='userWorkedHours'>Hours Worked:</h3>
-                    <UserPayrollReportForm />
-                </div>
-                <div className='payroll-table'>
-                    <UserPayrollTable />
+        <>
+            <section className='dashboard'>
+                <div className='container'>
+                    <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Period</TableCell>
+                                <TableCell>Pay</TableCell>
+                                <TableCell>Deductions</TableCell>
+                                <TableCell>Net Pay</TableCell>
+                                <TableCell>View</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {payrolls?.map((payroll, k) => (
+                                <TableRow key={k}>
+                                    <TableCell>{payroll.period}</TableCell>
+                                    <TableCell>${payroll.pay}</TableCell>
+                                    <TableCell>${payroll.deductions}</TableCell>
+                                    <TableCell>${payroll.netPay}</TableCell>
+                                    <TableCell>
+                                        <Button>View</Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 </div>
             </section>
-        </div>
+        </>
     );
 };
