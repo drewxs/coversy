6d514f47e10f4d6d0d5d7e7b5cc03f7e97621b99
Tomnguyen2604 <63090801+Tomnguyen2604@users.noreply.github.com>
2022-03-21 16:59:28 -0600

    testing updated

---
+++ client/src/test/user.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const style = {
+    width: '100%',
+    margin: '2rem 0rem 0rem 15rem',
+};
+const user = {
+    name: 'Gia Hoa Nguyen',
+    email: 'riderdecade007@gmail.com',
+};
