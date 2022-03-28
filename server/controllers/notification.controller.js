55b49fb28b44b186a2662949573b03f02b72fc86
Aedan Higgins <Aedan.Higgins@edu.sait.ca>
2022-03-28 12:20:13 -0600

    Fix minor issues based on feedback

--- server/controllers/notification.controller.js
+++ server/controllers/notification.controller.js
@@ -2,53 +2,51 @@ const Notification = require('../models/notification.model');
 const escape = require('escape-html');
 
 exports.createNotification = async (sender, receiver, msg) => {
-	const query = {
-		sender: sender,
-		receiver: receiver,
-		message: msg,
-		read: false,
-	};
+    const query = {
+        sender: sender,
+        receiver: receiver,
+        message: msg,
+        read: false,
+    };
 
-	try {
-		const total = await Notification.find({ receiver: receiver._id })
-			.lean()
-			.sort('-date');
-		if (total.length > 10) {
-			await Notification.findByIdAndDelete(total[total.length - 1]._id);
-		}
+    try {
+        const total = await Notification.find({ receiver: receiver._id })
+            .lean()
+            .sort('-date');
+        if (total.length > 10) {
+            await Notification.findByIdAndDelete(total[total.length - 1]._id);
+        }
 
-		await Notification.create(query);
-	} catch (err) {
-		console.log(err);
-	}
+        await Notification.create(query);
+    } catch (err) {}
 };
 
 /**
  *
- * @desc This function gets a notification by ID.
- * @route GET /notification/:notificationId
- * @access PUBLIC
+ * @desc This function gets notifications for a user
+ * @route GET /notification/
+ * @access USER
  */
 exports.getNotifications = (req, res) => {
-	Notification.find({ receiver: req.user._id })
-		.lean()
-		.then((notification) => res.status(200).json(notification))
-		.catch((err) => res.status(400).json(err));
+    Notification.find({ receiver: req.user._id })
+        .lean()
+        .then((notification) => res.status(200).json(notification))
+        .catch((err) => res.status(400).json(err));
 };
 
 /**
  *
  * @desc This function updates the read parameter of a notification.
  * @route POST /notification/:notificationId
- * @access PUBLIC
+ * @access USER
  */
 exports.readNotification = (req, res) => {
-	const updateQuery = {
-		read: true,
-	};
-	const notificationId = escape(req.params.notificationId);
+    const updateQuery = {
+        read: true,
+    };
+    const notificationId = escape(req.params.notificationId);
 
-	Notification.findByIdAndUpdate(notificationId, updateQuery, { new: true })
-		.then((notification) => res.status(200).json(notification))
-		.catch((err) => res.status(400).json(err));
+    Notification.findByIdAndUpdate(notificationId, updateQuery, { new: true })
+        .then((notification) => res.status(200).json(notification))
+        .catch((err) => res.status(400).json(err));
 };
