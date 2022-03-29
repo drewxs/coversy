50a65bf89555807dda60cf671ed01c1859e69328
kito0 <drewshah0@gmail.com>
2022-03-29 01:11:54 -0600

    Restructure server

--- server/index.js
+++ server/index.js
@@ -1,28 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(process.env.CONNECTION_URL, {
-		useNewUrlParser: true,
-	})
-	.then(() =>
-		app.listen(PORT, () =>
-			console.log(`Server is running on port: ${PORT}`)
-		)
-	)
-	.catch((err) => console.log(err.message));
+    .connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server is running on port: ${PORT}`)
+        )
+    )
+    .catch((err) => console.log(err.message));
 
 app.use(express.json());
 app.use(
-	cors({
-		origin: process.env.CLIENT_URL,
-		optionsSuccessStatus: 200,
-	})
+    cors({
+        origin: process.env.CLIENT_URL,
+        optionsSuccessStatus: 200,
+    })
 );
 
 app.use('/api/auth', require('./routes/auth.route'));
