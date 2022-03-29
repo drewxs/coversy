775ec13d20c3a3d9bc50a90f60f790e7e951d5c7
kito0 <drewshah0@gmail.com>
2022-03-28 21:50:48 -0600

    Add updateUserAsAdmin endpoint

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,12 +1,15 @@
 const User = require('../models/user.model');
 const escape = require('escape-html');
 const aws = require('aws-sdk');
-const { updateValidation } = require('../util/validation');
+const {
+    updateValidation,
+    updateValidationAdmin,
+} = require('../util/validation');
 
 aws.config.update({
-	secretAccessKey: process.env.S3_ACCESS_SECRET,
-	accessKeyId: process.env.S3_ACCESS_KEY,
-	region: process.env.S3_DEFAULT_REGION,
+    secretAccessKey: process.env.S3_ACCESS_SECRET,
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    region: process.env.S3_DEFAULT_REGION,
 });
 const s3 = new aws.S3();
 
@@ -16,12 +19,12 @@ const s3 = new aws.S3();
  * @access User
  */
 exports.getUserById = async (req, res) => {
-	const userId = escape(req.params.userId);
+    const userId = escape(req.params.userId);
 
-	User.findById(userId)
-		.populate('site')
-		.then((user) => res.status(200).json(user))
-		.catch((err) => res.status(400).json(err));
+    User.findById(userId)
+        .populate('site')
+        .then((user) => res.status(200).json(user))
+        .catch((err) => res.status(400).json(err));
 };
 
 /**
@@ -30,11 +33,11 @@ exports.getUserById = async (req, res) => {
  * @access Admin
  */
 exports.getUsersBySite = async (req, res) => {
-	const siteId = escape(req.params.siteId);
+    const siteId = escape(req.params.siteId);
 
-	User.find({ site: siteId, type: 2 })
-		.then((users) => res.status(200).json(users))
-		.catch((err) => res.status(400).json(err));
+    User.find({ site: siteId, type: 2 })
+        .then((users) => res.status(200).json(users))
+        .catch((err) => res.status(400).json(err));
 };
 
 /**
@@ -43,28 +46,54 @@ exports.getUsersBySite = async (req, res) => {
  * @access User
  */
 exports.updateUserById = (req, res) => {
-	const updateQuery = {};
-	if (req.body.firstName) updateQuery.firstName = escape(req.body.firstName);
-	if (req.body.lastName) updateQuery.lastName = escape(req.body.lastName);
-	if (req.body.middleInitial)
-		updateQuery.middleInitial = escape(req.body.middleInitial);
-	if (req.body.phone) updateQuery.phone = escape(req.body.phone);
-	if (req.body.email) updateQuery.email = escape(req.body.email);
-	if (req.body.avatar) updateQuery.avatar = escape(req.body.avatar);
-	if (req.body.bio) updateQuery.bio = escape(req.body.bio);
-	if (req.body.verified) updateQuery.verified = escape(req.body.verified);
-	if (req.body.site) updateQuery.site = escape(req.body.site);
-	const userId = escape(req.params.userId);
+    const updateQuery = {};
+    if (req.body.firstName) updateQuery.firstName = escape(req.body.firstName);
+    if (req.body.lastName) updateQuery.lastName = escape(req.body.lastName);
+    if (req.body.phone) updateQuery.phone = escape(req.body.phone);
+    if (req.body.email) updateQuery.email = escape(req.body.email);
+    if (req.body.avatar) updateQuery.avatar = escape(req.body.avatar);
+    if (req.body.bio) updateQuery.bio = escape(req.body.bio);
+    if (req.body.verified) updateQuery.verified = escape(req.body.verified);
+    if (req.body.site) updateQuery.site = escape(req.body.site);
+    const userId = escape(req.params.userId);
 
-	const { error } = updateValidation(updateQuery);
-	if (error) return res.status(400).json(error.details[0].message);
+    const { error } = updateValidation(updateQuery);
+    if (error) return res.status(400).json(error.details[0].message);
 
-	User.findByIdAndUpdate(userId, updateQuery, { new: true })
-		.then((user) => {
-			if (!user) res.status(404).json('Error: User ID does not exist.');
-			res.status(200).json(user);
-		})
-		.catch((err) => res.status(400).json(err));
+    User.findByIdAndUpdate(userId, updateQuery, { new: true })
+        .then((user) => {
+            if (!user) res.status(404).json('Error: User ID does not exist.');
+            res.status(200).json(user);
+        })
+        .catch((err) => res.status(400).json(err));
+};
+
+/**
+ * @desc This function updates users by id as admin
+ * @route PUT /user/:userId/admin
+ * @access Admin
+ */
+exports.updateUserAsAdmin = async (req, res) => {
+    const userId = escape(req.params.userId);
+    const updateQuery = {};
+    if (req.body.firstName) updateQuery.firstName = escape(req.body.firstName);
+    if (req.body.lastName) updateQuery.lastName = escape(req.body.lastName);
+    if (req.body.hourlyRate)
+        updateQuery.hourlyRate = escape(req.body.hourlyRate);
+    if (req.body.taxRate) updateQuery.taxRate = escape(req.body.taxRate);
+
+    const { error } = updateValidationAdmin(updateQuery);
+    if (error) return res.status(400).json(error.details[0].message);
+
+    try {
+        const user = await User.findByIdAndUpdate(userId, updateQuery, {
+            new: true,
+        });
+        if (!user) res.status(404).json('Error: User ID does not exist.');
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(400).json(err.message);
+    }
 };
 
 /**
@@ -73,23 +102,23 @@ exports.updateUserById = (req, res) => {
  * @access Admin
  */
 exports.toggleUserActivatedById = async (req, res) => {
-	const userId = escape(req.params.userId);
-	let user;
-	try {
-		user = await User.findById(userId);
-	} catch (err) {
-		return res.status(404).send('User not found.');
-	}
+    const userId = escape(req.params.userId);
+    let user;
+    try {
+        user = await User.findById(userId);
+    } catch (err) {
+        return res.status(404).send('User not found.');
+    }
 
-	if (!user) return res.status(404).json('Error: User ID does not exist.');
-	if (user.site != req.user.site)
-		return res.status(404).json('User is not part of this site.');
+    if (!user) return res.status(404).json('Error: User ID does not exist.');
+    if (user.site != req.user.site)
+        return res.status(404).json('User is not part of this site.');
 
-	const updateQuery = { activated: !user.activated };
+    const updateQuery = { activated: !user.activated };
 
-	User.findByIdAndUpdate(userId, updateQuery, { new: true })
-		.then((user) => res.status(200).json(user))
-		.catch((err) => res.status(400).json(err));
+    User.findByIdAndUpdate(userId, updateQuery, { new: true })
+        .then((user) => res.status(200).json(user))
+        .catch((err) => res.status(400).json(err));
 };
 
 /**
@@ -98,14 +127,14 @@ exports.toggleUserActivatedById = async (req, res) => {
  * @access PUBLIC
  */
 exports.getProfilePicture = (req, res) => {
-	const fileKey = escape(req.params.key);
-	const downloadParams = {
-		Key: fileKey,
-		Bucket: process.env.S3_PROFILE_BUCKET,
-	};
+    const fileKey = escape(req.params.key);
+    const downloadParams = {
+        Key: fileKey,
+        Bucket: process.env.S3_PROFILE_BUCKET,
+    };
 
-	const readStream = s3.getObject(downloadParams).createReadStream();
-	readStream.pipe(res);
+    const readStream = s3.getObject(downloadParams).createReadStream();
+    readStream.pipe(res);
 };
 
 /**
@@ -114,35 +143,35 @@ exports.getProfilePicture = (req, res) => {
  * @access USER
  */
 exports.updateProfilePicture = async (req, res) => {
-	if (!req.file) return res.status(400).send('No image uploaded');
-	const userId = escape(req.params.userId);
+    if (!req.file) return res.status(400).send('No image uploaded');
+    const userId = escape(req.params.userId);
 
-	const updateQuery = { avatar: 'user/images/' + req.file.key };
+    const updateQuery = { avatar: 'user/images/' + req.file.key };
 
-	User.findByIdAndUpdate(userId, updateQuery, { new: true })
-		.then((user) => res.status(200).json(user))
-		.catch((err) => res.status(400).json(err));
+    User.findByIdAndUpdate(userId, updateQuery, { new: true })
+        .then((user) => res.status(200).json(user))
+        .catch((err) => res.status(400).json(err));
 };
 
 exports.deleteProfilePicture = (req, res) => {
-	const userId = escape(req.params.userId);
-	const updateQuery = { avatar: undefined };
+    const userId = escape(req.params.userId);
+    const updateQuery = { avatar: undefined };
 
-	User.findByIdAndUpdate(userId, updateQuery)
-		.then((user) => {
-			if (user.avatar) {
-				const fileKey = user.avatar.split('/')[2];
-				const deleteParams = {
-					Key: fileKey,
-					Bucket: process.env.S3_PROFILE_BUCKET,
-				};
-				try {
-					s3.deleteObject(deleteParams);
-					res.status(200).json(user);
-				} catch (err) {
-					res.status(400).json(err);
-				}
-			}
-		})
-		.catch((err) => res.status(400).json(err));
+    User.findByIdAndUpdate(userId, updateQuery)
+        .then((user) => {
+            if (user.avatar) {
+                const fileKey = user.avatar.split('/')[2];
+                const deleteParams = {
+                    Key: fileKey,
+                    Bucket: process.env.S3_PROFILE_BUCKET,
+                };
+                try {
+                    s3.deleteObject(deleteParams);
+                    res.status(200).json(user);
+                } catch (err) {
+                    res.status(400).json(err);
+                }
+            }
+        })
+        .catch((err) => res.status(400).json(err));
 };
