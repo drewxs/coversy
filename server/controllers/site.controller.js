988d40d396e8492735ed0f194a327e1b133c6e20
Liam Zimmerman <Liam.Zimmerman@edu.sait.ca>
2022-03-28 19:30:18 -0600

    Update shift endpoints

--- server/controllers/site.controller.js
+++ server/controllers/site.controller.js
@@ -1,5 +1,6 @@
 const Site = require('../models/site.model');
 const escape = require('escape-html');
+const { siteValidation } = require('../util/validation');
 
 /**
  *
@@ -8,18 +9,22 @@ const escape = require('escape-html');
  * @access Admin
  */
 exports.createSite = async (req, res) => {
-	const location = {
-		name: escape(req.body.name),
-		address: {
-			street: escape(req.body.address.street),
-			zip: escape(req.body.address.zip),
-			city: escape(req.body.address.city),
-			province: escape(req.body.address.province),
-		},
-	};
-	Site.create(location)
-		.then((site) => res.status(200).json(site))
-		.catch((err) => res.status(400).json(err));
+    const location = {
+        name: escape(req.body.name),
+        address: {
+            street: escape(req.body.address.street),
+            zip: escape(req.body.address.zip),
+            city: escape(req.body.address.city),
+            province: escape(req.body.address.province),
+        },
+    };
+
+    const { error } = siteValidation(location);
+    if (error) return res.status(400).json(error.details[0].message);
+
+    Site.create(location)
+        .then((site) => res.status(201).json(site))
+        .catch((err) => res.status(400).json(err));
 };
 
 /**
@@ -29,9 +34,9 @@ exports.createSite = async (req, res) => {
  * @access Admin
  */
 exports.getAllSites = async (req, res) => {
-	Site.find()
-		.then((site) => res.status(200).json(site))
-		.catch((err) => res.status(400).json(err));
+    Site.find()
+        .then((site) => res.status(200).json(site))
+        .catch((err) => res.status(400).json(err));
 };
 
 /**
@@ -41,11 +46,11 @@ exports.getAllSites = async (req, res) => {
  * @access Admin
  */
 exports.getSiteById = async (req, res) => {
-	const siteId = escape(req.params.siteId);
+    const siteId = escape(req.params.siteId);
 
-	Site.findById(siteId)
-		.then((site) => res.status(200).json(site))
-		.catch((err) => res.status(400).json(err));
+    Site.findById(siteId)
+        .then((site) => res.status(200).json(site))
+        .catch((err) => res.status(400).json(err));
 };
 
 /**
@@ -55,21 +60,24 @@ exports.getSiteById = async (req, res) => {
  * @access Admin
  */
 exports.updateSiteById = async (req, res) => {
-	const updateQuery = {};
-	if (req.body.name) {
-		updateQuery.name = escape(req.body.name);
-	}
-	if (req.body.address) {
-		updateQuery.address = {
-			street: escape(req.body.address.street),
-			zip: escape(req.body.address.zip),
-			city: escape(req.body.address.city),
-			province: escape(req.body.address.province),
-		};
-	}
-	const siteId = escape(req.params.siteId);
+    const updateQuery = {};
+    if (req.body.name) {
+        updateQuery.name = escape(req.body.name);
+    }
+    if (req.body.address) {
+        updateQuery.address = {
+            street: escape(req.body.address.street),
+            zip: escape(req.body.address.zip),
+            city: escape(req.body.address.city),
+            province: escape(req.body.address.province),
+        };
+    }
+    const siteId = escape(req.params.siteId);
 
-	Site.findByIdAndUpdate(siteId, updateQuery, { new: true })
-		.then((site) => res.status(200).json(site))
-		.catch((err) => res.status(400).json(err));
+    const { error } = siteValidation(updateQuery);
+    if (error) return res.status(400).json(error.details[0].message);
+
+    Site.findByIdAndUpdate(siteId, updateQuery, { new: true })
+        .then((site) => res.status(200).json(site))
+        .catch((err) => res.status(400).json(err));
 };
