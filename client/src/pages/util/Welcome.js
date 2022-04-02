77591939ae7e6c7e6c033583f2d55522b45eb329
kito0 <drewshah0@gmail.com>
2022-04-02 17:35:13 -0600

    Update services

--- client/src/pages/util/Welcome.js
+++ client/src/pages/util/Welcome.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { verifyUser } from 'services/auth.service';
+import { confirmUser } from 'services/auth.service';
 
 export const Welcome = (props) => {
-	if (props.match.path === '/confirm/:confirmationCode') {
-		verifyUser(props.match.params.confirmationCode);
-	}
+    if (props.match.path === '/confirm/:confirmationCode') {
+        confirmUser(props.match.params.confirmationCode);
+    }
 
-	return (
-		<div className='container'>
-			<h1>Account confirmed!</h1>
-			<Link to={'/login'}>Please Login</Link>
-		</div>
-	);
+    return (
+        <div className='container'>
+            <h1>Account confirmed!</h1>
+            <Link to={'/login'}>Please Login</Link>
+        </div>
+    );
 };
--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const verifyUser = (code) => {
-	return axios
-		.get(`${process.env.REACT_APP_API_URL}/auth/confirm/${code}`)
-		.then((res) => {
-			return res.data;
-		});
+export const confirmUser = (code) => {
+    return axios
+        .get(`${process.env.REACT_APP_API_URL}/auth/confirm/${code}`)
+        .then((res) => {
+            return res.data;
+        });
 };
