43b03ac0714762627f5207a1f1b5c0e4b50efe2c
Zhenyi <Zhenyi@DESKTOP-IT8GUBP>
2022-04-04 21:15:10 -0600

    Adding add shift modal

--- client/src/pages/admin/AdminShifts.js
+++ client/src/pages/admin/AdminShifts.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { GetShifts, AddShift } from 'redux/shift';
+import { FetchUsers } from 'redux/admin';
 import {
     Box,
     Typography,
@@ -11,14 +12,21 @@ import {
     TableCell,
     TableHead,
     TableRow,
+    TextField,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
 } from '@mui/material';
 import Papa from 'papaparse';
 import moment from 'moment';
+import MobileDateTimePicker from '@mui/lab';
 
 export const AdminShifts = () => {
     const shifts = useSelector((state) => state.shift.shifts);
 
     const [open, setOpen] = useState(false);
+    const [openadd, setOpenadd] = useState(false);
     const [file, setFile] = useState();
     // const [date, setDate] = useState(new Date());
 
@@ -40,9 +48,23 @@ export const AdminShifts = () => {
         }
     };
 
+    const handleAddShift = () => {};
+
+    const [user, setUser] = useState('');
+    const [subject, setSubject] = useState(null);
+    const [shiftstartTime, setshiftstartTime] = useState(null);
+    const [shiftendTime, setshiftendTime] = useState(null);
+
+    const users = useSelector((state) => state.admin.users);
+    const admin = useSelector((state) => state.user.user);
+
     useEffect(() => {
         GetShifts();
     }, []);
+
+    useEffect(() => {
+        if (admin.site._id) FetchUsers(admin.site._id);
+    }, [admin]);
 
     return (
         <>
@@ -54,6 +76,13 @@ export const AdminShifts = () => {
                         onClick={() => setOpen(true)}
                     >
                         Upload Schedule
+                    </Button>
+                    <Button
+                        sx={{ mb: 2 }}
+                        variant='contained'
+                        onClick={() => setOpenadd(true)}
+                    >
+                        ADD
                     </Button>
                     <Table>
                         <TableHead>
@@ -88,7 +117,6 @@ export const AdminShifts = () => {
                     </Table>
                 </div>
             </section>
-
             {/* Upload Schedule Modal */}
             <Modal open={open} onClose={() => setOpen(false)}>
                 <Box
@@ -119,6 +147,72 @@ export const AdminShifts = () => {
                     >
                         Upload
                     </Button>
+                </Box>
+            </Modal>
+            // ADD Shift Modal
+            <Modal open={openadd} onClose={() => setOpenadd(false)}>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 400,
+                        bgcolor: 'background.paper',
+                        boxShadow: 24,
+                        p: 4,
+                    }}
+                >
+                    <Typography variant='h6'>Add a shift</Typography>
+                    <form>
+                        <FormControl fullWidth>
+                            <InputLabel>User</InputLabel>
+                            <Select
+                                className='input'
+                                value={user}
+                                onChange={(e) => setUser(e.target.value)}
+                            >
+                                {users?.map((user, k) => (
+                                    <MenuItem value={user._id} key={k}>
+                                        {user.Firstname}
+                                        {user.LastName}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <TextField
+                            value={subject}
+                            onChange={(e) => setSubject(e.target.value)}
+                            fullWidth
+                            label='Subject'
+                            placeholder='Subject'
+                        />
+                        <MobileDateTimePicker
+                            label='Start Time'
+                            value={shiftstartTime}
+                            onChange={(e) => setshiftstartTime(e.target.value)}
+                        />
+                        <MobileDateTimePicker
+                            label='End Time'
+                            value={shiftendTime}
+                            onChange={(e) => setshiftendTime(e.target.value)}
+                        />
+                        <Button
+                            variant='contained'
+                            color='primary'
+                            sx={{ mt: '1rem' }}
+                            onClick={() => handleAddShift()}
+                        >
+                            Add
+                        </Button>
+                        <Button
+                            sx={{ mt: '1rem' }}
+                            variant='outlined'
+                            onClick={() => setOpenadd(false)}
+                        >
+                            Cancel
+                        </Button>
+                    </form>
                 </Box>
             </Modal>
         </>
