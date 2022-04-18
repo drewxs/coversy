da60a83f11f0ab3faf7af54f263d6c672b090356
kito0 <drewshah0@gmail.com>
2022-04-18 17:07:06 -0600

    Add monochromatic colors to exports

--- client/src/config/theme.js
+++ client/src/config/theme.js
@@ -2,14 +2,16 @@ import { createTheme } from '@mui/material/styles';
 import variables from 'scss/abstract/_variables.scss';
 
 export const theme = createTheme({
-	typography: {
-		fontFamily: variables.fontBody,
-	},
-	palette: {
-		primary: { main: variables.primary, contrastText: '#fff' },
-		secondary: { main: variables.secondary, contrastText: '#fff' },
-		contrastThreshold: 3,
-		tonalOffset: 0.2,
-		spacing: 2,
-	},
+    typography: {
+        fontFamily: variables.fontBody,
+    },
+    palette: {
+        primary: { main: variables.primary, contrastText: '#fff' },
+        secondary: { main: variables.secondary, contrastText: '#fff' },
+        black: { main: variables.black, contrastText: variables.white },
+        white: { main: variables.white, contrastText: variables.black },
+        contrastThreshold: 3,
+        tonalOffset: 0.2,
+        spacing: 2,
+    },
 });
