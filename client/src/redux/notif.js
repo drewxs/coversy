ecf2490d5d125fbe9d89eb52f7c9faa1ed6d9215
kito0 <drewshah0@gmail.com>
2022-04-18 18:13:06 -0600

    Update docs

--- client/src/redux/notif.js
+++ client/src/redux/notif.js
@@ -21,9 +21,7 @@ export const GetNotifications = async () => {
     store.dispatch(loadingNotifications);
     await axios
         .get(`${api}/notification`, {
-            headers: {
-                'auth-token': localStorage.getItem('auth-token'),
-            },
+            headers: { 'auth-token': localStorage.getItem('auth-token') },
         })
         .then((res) => store.dispatch(setNotifications(res.data)))
         .catch((err) => console.error(err));
@@ -39,20 +37,24 @@ export const ReadNotifications = async () => {
     store.dispatch(loadingNotifications);
     await axios
         .put(`${api}/notification`, null, {
-            headers: {
-                'auth-token': localStorage.getItem('auth-token'),
-            },
+            headers: { 'auth-token': localStorage.getItem('auth-token') },
         })
         .then((res) => store.dispatch(readNotifications(res.data)))
         .catch((err) => console.error(err));
 };
+
+/**
+ * Deletes a notification.
+ *
+ * @function
+ * @async
+ * @param {string} notifId - Object id of the notification.
+ */
 export const DeleteNotification = async (notifId) => {
     store.dispatch(loadingNotifications);
     await axios
         .delete(`${api}/notification/${notifId}`, {
-            headers: {
-                'auth-token': localStorage.getItem('auth-token'),
-            },
+            headers: { 'auth-token': localStorage.getItem('auth-token') },
         })
         .then((res) => store.dispatch(removeNotification(res.data)))
         .catch((err) => console.error(err));
