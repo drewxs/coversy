c6e1e52876b9547cd781fc310306c521c1c4c0ac
kito0 <drewshah0@gmail.com>
2022-04-27 14:22:33 -0600

    build: Add scss to linting scope

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,7 @@
 export { ConfirmationModal } from './ConfirmationModal';
 export { Errors } from './Errors';
 export { Nav } from './Nav';
+export { NavLink } from './NavLink';
 export { UserShift } from './UserShift';
 export { Notifications } from './Notifications';
 export { UnactivatedModal } from './UnactivatedModal';
